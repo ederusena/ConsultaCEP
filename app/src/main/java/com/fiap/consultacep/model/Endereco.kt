bde11f4d8365e20aa74f1fd3cec4f0d0cc4573fc
Eder Soares Sena <eder.sena@live.com>
2023-08-31 10:22:10 -0300

    Initial commit

---
+++ app/src/main/java/com/fiap/consultacep/model/Endereco.kt
@@ -0,0 +1,12 @@
+package com.fiap.consultacep.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Endereco(
+    val cep: String = "",
+    @SerializedName("logradouro") val rua: String = "",
+    @SerializedName("localidade") val cidade: String = "",
+    val bairro: String = "",
+    val uf: String = ""
+)
---
+++ app/src/main/java/com/fiap/consultacep/service/CepService.kt
@@ -0,0 +1,23 @@
+package com.fiap.consultacep.service
+
+
+import com.fiap.consultacep.model.Endereco
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface CepService {
+
+    //https://viacep.com.br/ws/01001000/json/
+    @GET("{cep}/json/")
+    fun getEnderecoByCep(@Path("cep") cep: String): Call<Endereco>
+
+    //https://viacep.com.br/ws/RS/Porto%20Alegre/Domingos/json/
+    @GET("{uf}/{cidade}/{rua}/json/")
+    fun getEnderecosByUfCidade(
+        @Path("uf") uf: String,
+        @Path("cidade") cidade: String,
+        @Path("rua") rua: String
+    ): List<Call<Endereco>>
+
+}
